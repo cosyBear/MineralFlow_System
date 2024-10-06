040dd3e3c517ffa0f634e119c97b7f2409640831
ali <ali.alzamili@student.kdg.be>
2024-10-06 17:43:59 +0200

    data,sql

--- LandBoundedContext/src/main/resources/l/data.sql
+++ LandBoundedContext/src/main/resources/l/data.sql
@@ -4,9 +4,14 @@ INSERT INTO appointments (id, license_plate, seller_id, material_type_entity, ti
                                                                                                  (3, 'ABC1003', UUID_TO_BIN('ff531b87-ba50-4b54-8666-5d860c4bd919'), 'IRON', '2024-09-23 11:00:00', 0);
 
 
-INSERT INTO ware_house_entity(warehouse_id, seller_id, material_type, amount_of_material)
+-- Set the seller_id to a known UUID
+SET @seller_id = UNHEX(REPLACE('8d50dbe3-68a4-4afc-a242-13818629ac9d', '-', ''));
+
+-- Insert 5 warehouses for the seller, each with a unique warehouse_id and corresponding material type (using ordinal values)
+INSERT INTO ware_house_entity (warehouse_id, seller_id, material_type, amount_of_material)
 VALUES
-    (UNHEX(REPLACE('123e4567-e89b-12d3-a456-426614174000', '-', '')), -- Convert Warehouse UUID to binary(16)
-     UNHEX(REPLACE('123e4567-e89b-12d3-a456-426614174001', '-', '')), -- Convert Seller UUID to binary(16)
-     1,  -- Assuming IRON material type is represented by 1
-     10.0); -- Amount of material
+    (UNHEX(REPLACE('f8a9f9c0-45e2-4f32-809f-2d4a73c4083b', '-', '')), @seller_id, 1, 0.0), -- IRON (MaterialType index 1)
+    (UNHEX(REPLACE('c0e9a5a3-bbfd-4e73-bef7-9a2a8bce394b', '-', '')), @seller_id, 0, 0.0), -- GYPSUM (MaterialType index 0)
+    (UNHEX(REPLACE('aedcb3a5-f2a6-4ed8-8090-0d92c3d6955c', '-', '')), @seller_id, 2, 0.0), -- CEMENT (MaterialType index 2)
+    (UNHEX(REPLACE('acdd75b0-0712-4ef1-870f-67a8d0d7bc18', '-', '')), @seller_id, 3, 0.0), -- PETCOKE (MaterialType index 3)
+    (UNHEX(REPLACE('b03f68bb-6606-482d-8007-dc7f94acb1e5', '-', '')), @seller_id, 4, 0.0); -- SLAG (MaterialType index 4)
