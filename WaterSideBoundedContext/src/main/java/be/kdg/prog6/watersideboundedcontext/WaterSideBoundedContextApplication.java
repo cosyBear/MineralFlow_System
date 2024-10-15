55a77483789a47117457d6215e9cf44ac8efce22
Kevin Smeyers <kevin.smeyers@one16.be>
2024-10-15 10:08:48 +0200

    ?

---
+++ WaterSideBoundedContext/src/main/java/be/kdg/prog6/watersideboundedcontext/WaterSideBoundedContextApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.prog6.watersideboundedcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterSideBoundedContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterSideBoundedContextApplication.class, args);
+    }
+
+}
